258479cafd0a1f30952434d7ac164af4b0f03941
Michael Hanley <pasaquali@gmail.com>
2017-01-31 21:46:39 -0800

    documentation

--- src/app/search-bar.service.ts
+++ src/app/search-bar.service.ts
@@ -4,12 +4,15 @@ import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class SearchBarService {
+  //set the variable to be the source string
   private searchTermSource = new Subject<string>();
+  //set your stream variable to watch for changes to the source
   searchTerm$ = this.searchTermSource.asObservable();
   data;
   constructor(private http: Http) { }
 
   onSearch(term) {
+    //push change to source
     this.searchTermSource.next(term);
   }
 }
