abc9e39175d8aa9a08bb32740d80dc212ccf9704
Michael Hanley <pasaquali@gmail.com>
2017-02-06 19:55:20 -0800

    added stupid simple routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import {SearchBarService} from './search-bar.service';
 import { ItemDisplayComponent } from './item-display/item-display.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const appRoutes: Routes = [
+  { path: 'Item-Search', component: ItemDisplayComponent },
+  { path: '',   redirectTo: '/Item-Search', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
@@ -19,7 +24,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [SearchBarService],
   bootstrap: [AppComponent]
